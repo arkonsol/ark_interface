b8c2c7b0a453ea94ef83bfcbd03dfd8e8cf48d11
Okereke Clement Kalu <inspikalu@gmail.com>
2024-09-17 19:09:26 +0100

    Added Newsletter component

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,6 +2,7 @@
 import DashboardHeader from "../components/getstarted/DashboardHeader";
 import DashboardSearch from "../components/create/DashboardSearch";
 import DashboardTables from "../components/create/DashboardTables";
+import NewsLetter from "../components/landing/NewsLetter";
 
 export default function Dashboard() {
   return (
@@ -10,6 +11,8 @@ export default function Dashboard() {
       <main className="">
         <DashboardSearch />
         <DashboardTables />
+        <NewsLetter />
+
         {/* Add your dashboard content here */}
       </main>
     </div>
